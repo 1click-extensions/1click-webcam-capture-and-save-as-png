0f230591b8f53e665bbf4d68c53eb16cf742d3b2
Itsick <any.site.n.site@gmail.com>
2018-04-26 22:41:45 +0300

    change menifest

--- js/webcam.js
+++ js/webcam.js
@@ -1,9 +1,9 @@
 localstream = null;
 captureButton = document.getElementById('capture');
-h2 = document.getElementById('h2');
 player = document.getElementById('vid');
 canvas = document.getElementById('canvas');
 context = canvas.getContext('2d');
+h2 = document.getElementById('h2');
 h2.innerText = chrome.i18n.getMessage("h2_title");
 captureButton.innerText = chrome.i18n.getMessage("save_picture");
 if (navigator.webkitGetUserMedia!=null) {
@@ -26,6 +26,19 @@ if (navigator.webkitGetUserMedia!=null) {
  captureButton.addEventListener('click', () => {
     // Draw the video frame to the canvas.
     context.drawImage(player, 0, 0, canvas.width, canvas.height);
+
+    canvas.toBlob(function(blob){
+            url = URL.createObjectURL(blob);
+        chrome.downloads.download({
+          url: url,
+          filename : getfilenameByExtention()
+        });
+
+    },'image/png');
+
+  });
+
+  function getfilenameByExtention(){
     var now = new Date(),
         month = now.getMonth() + 1,
         day = now.getDate(),
@@ -34,14 +47,6 @@ if (navigator.webkitGetUserMedia!=null) {
         minutes = now.getMinutes(),
         hour = now.getHours(),
         filename = chrome.runtime.getManifest().name + '--' + [day,month,year].join('-') + '--' +[hour,minutes,seconds].join('-');
-    filename = filename.replace(/(\s|\t)+/g,'-').toLocaleLowerCase();
-    canvas.toBlob(function(blob){
-            url = URL.createObjectURL(blob);
-        chrome.downloads.download({
-          url: url,
-          filename : filename
-        });
-
-    },'image/png');
-
-  });
+        filename = filename.replace(/(\s|\t)+/g,'-').toLocaleLowerCase() + '.png';
+    return filename;
+  }
