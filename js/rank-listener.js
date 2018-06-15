ce08dab791a2ff60d2036714e8d16ee23b7fdd4f
Itsick <any.site.n.site@gmail.com>
2018-06-15 13:36:18 +0300

    with poup

--- js/rank-listener.js
+++ js/rank-listener.js
@@ -3,11 +3,15 @@ chrome.runtime.onMessage.addListener(function (data, sender, callback) {
 		//needs permission to do it
 		case 'injectJs':
 			//console.log('clicke',tab.id, chrome.runtime.getURL('js/rank-reciever.js'));
- 			chrome.tabs.executeScript(sender.tab.id,{code : "openRankPopupWhenPossible = true;"});
-			chrome.tabs.executeScript(sender.tab.id,{file:'js/rank-reciever.js'});
+ 			
+			chrome.tabs.executeScript(sender.tab.id,{file:'js/extention-data.js'});
+			chrome.tabs.executeScript(sender.tab.id,{file:'js/show-popup.js'}, function(){
+				chrome.tabs.executeScript(sender.tab.id,{code:"checkIfRankNeededAndAndAddRank()"})
+			});
 			//console.log(sender.tab, sender.tab.id);
 			break;
 		case 'checkIfNeedRating':
+		console.log(localStorage.getItem('rankRequested'));
 			callback(!localStorage.getItem('rankRequested'));
 			break;
 		case 'rankRequested':
